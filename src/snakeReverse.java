079a3df5f8d2df55f968907405d85aa61bddad64
Aneek Mukherjee <146527647+Aneekcodes@users.noreply.github.com>
2026-06-28 17:06:45 +0530

    2D Array

--- src/snakeReverse.java
+++ src/snakeReverse.java
@@ -1,3 +1,24 @@
 public class snakeReverse {
+    static void main() {
+        int arr[][]={
+                {1,2,3,4},
+                {5,7,8,9},
+                {7,8,9,1},
+                {5,4,6,7}};
+        for(int i=0; i<arr.length; i++){
+            if(i%2==0){
+                for(int j=0; j< arr.length; j++){
+                    System.out.print(arr[j][i]+" ");
+                }
+            }
+            else{
+                for (int j = arr.length-1; j>=0;j--){
+                    System.out.print(arr[j][i]+" ");
+                }
+            }
+            System.out.println();
+
+        }
+    }
 
 }
